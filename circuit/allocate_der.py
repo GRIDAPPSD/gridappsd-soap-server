ba7fa8c44653b8bb9a22fc2922ed9134f9dda88d
temcdrm <Thomas.McDermott@pnnl.gov>
2021-12-21 08:46:58 -0800

    inserting commercial-scale DER and rooftop PV

---
+++ circuit/allocate_der.py
@@ -0,0 +1,76 @@
+from SPARQLWrapper import SPARQLWrapper2#, JSON
+import cimhub.CIMHubConfig as CIMHubConfig
+import sys
+import numpy as np
+
+def create_der_list (cfg_file, froot, mRID, outpath, pv_size, pv_penetration):
+  if outpath is not None:
+    froot = './{:s}/{:s}'.format (outpath, froot)
+  CIMHubConfig.ConfigFromJsonFile (cfg_file)
+  sparql = SPARQLWrapper2(CIMHubConfig.blazegraph_url)
+
+  np.random.seed (0)
+  numpv = 0
+  kw = 0.001 * pv_size
+  kva = kw / 0.898
+  kv = 0.208
+  kvar = 0.0
+
+  op = open (froot + '_der.dat', 'w')
+  print ('uuid_file,{:s}_der_uuid.dat'.format (froot), file=op)
+  print ('feederID,{:s}'.format (mRID), file=op)
+  print ('// name,bus,phases(ABCs1s2),type(Battery,Photovoltaic,SynchronousMachine),RatedkVA,RatedkV,kW,kVAR,RatedkWH,StoredkWH', file=op)
+  print ('// commercial-scale, three-phase DER connected to the primary feeder', file=op)
+  print ('PV_1,node_60,ABC,Photovoltaic,2230.0,4.16,2000.0,0.0', file=op)
+  print ('Bat_PV,node_60,ABC,Battery,2000.0,4.16,0.0,0.0,8000.0,4000.0', file=op)
+  print ('WTG_1,node_1,ABC,SynchronousMachine,2230.0,4.16,2000.0,0.0', file=op)
+  print ('Bat_WTG,node_1,ABC,Battery,2000.0,4.16,0.0,0.0,8000.0,4000.0', file=op)
+  print ('// adding rooftop PV sized {:.2f} kW at {:.2f}% penetration'.format (kw, 100.0 * pv_penetration), file=op)
+
+  fidselect = """ VALUES ?fdrid {\"""" + mRID + """\"}
+   ?s c:Equipment.EquipmentContainer ?fdr.
+   ?fdr c:IdentifiedObject.mRID ?fdrid. """
+
+  ####################### - EnergyConsumer
+  qstr = CIMHubConfig.prefix + """SELECT ?name ?bus ?basev ?p ?q ?conn ?cnt (group_concat(distinct ?phs;separator=":") as ?phases) WHERE {"""  + fidselect + """
+ ?s r:type c:EnergyConsumer.
+ ?s c:IdentifiedObject.name ?name.
+ ?s c:ConductingEquipment.BaseVoltage ?bv.
+ ?bv c:BaseVoltage.nominalVoltage ?basev.
+ ?s c:EnergyConsumer.customerCount ?cnt.
+ ?s c:EnergyConsumer.p ?p.
+ ?s c:EnergyConsumer.q ?q.
+ ?s c:EnergyConsumer.phaseConnection ?connraw.
+   bind(strafter(str(?connraw),\"PhaseShuntConnectionKind.\") as ?conn)
+ OPTIONAL {?ecp c:EnergyConsumerPhase.EnergyConsumer ?s.
+ ?ecp c:EnergyConsumerPhase.phase ?phsraw.
+   bind(strafter(str(?phsraw),\"SinglePhaseKind.\") as ?phs) }
+ ?t c:Terminal.ConductingEquipment ?s.
+ ?t c:Terminal.ConnectivityNode ?cn. 
+ ?cn c:IdentifiedObject.name ?bus
+}
+GROUP BY ?name ?bus ?basev ?p ?q ?cnt ?conn
+ORDER by ?p ?name
+  """
+#  print (qstr)
+  sparql.setQuery(qstr)
+  ret = sparql.query()
+#  print ('\nEnergyConsumer binding keys are:',ret.variables)
+  for b in ret.bindings:
+    #    print (b['bus'].value,b['basev'].value,b['phases'].value,b['p'].value)
+    if np.random.uniform (0, 1) <= pv_penetration:
+      numpv += 1
+      print ('Rooftop_{:d},{:s},s1s2,Photovoltaic,{:.3f},{:.3f},{:.3f},{:.3f}'.format (numpv, b['bus'].value, kva, kv, kw, kvar), file=op)
+
+  op.close()
+  print ('added {:d} PV sized {:.2f} kW for {:.2f}% penetration'.format (numpv, kw, 100.0 * pv_penetration))
+
+# run from command line for GridAPPS-D interop circuit
+if __name__ == '__main__':
+  cfg_file = 'cimhubconfig.json'
+  ckt_mRID = '_503D6E20-F499-4CC7-8051-971E23D0BF79'
+  froot = 'Transactive'
+  outpath = None
+  pv_size = 3000.0
+  pv_penetration = 0.9
+  create_der_list (cfg_file, froot, ckt_mRID, outpath, pv_size, pv_penetration)
--- circuit/make_circuit.py
+++ circuit/make_circuit.py
@@ -16,7 +16,7 @@ cwd = os.getcwd()
 
 cases = [
   {'root':froot, 'mRID':'503D6E20-F499-4CC7-8051-971E23D0BF79','glmvsrc': 2400.00,'bases':[4160.0],
-   'export_options':' -l=1.0 -z=1.0 -e=carson',
+   'export_options':' -l=1.0 -z=1.0 =h=1 -e=carson',
    'check_branches':[{'dss_link': 'LINE.LINE_L114', 'dss_bus': 'NODE_135', 'gld_link': 'LINE_LINE_L114', 'gld_bus': 'NODE_135'}]},
   ]
 
@@ -29,6 +29,12 @@ cimhub.clear_db (cfg_json)
 cmd = 'curl -D- -H "Content-Type: application/xml" --upload-file {:s}.xml -X POST '.format (froot) + CIMHubConfig.blazegraph_url
 os.system (cmd)
 cimhub.list_feeders ()
+
+# insert houses and DER
+cimhub.insert_houses (cfg_json, ckt_mRID, 3, 0, 'Transactive_house_uuids.json', 1.0)
+p1 = subprocess.Popen ('python3 allocate_der.py', shell=True)
+p1.wait()
+cimhub.insert_der (cfg_json, 'Transactive_der.dat')
 
 # insert measurements
 cimhub.list_measurables (cfg_json, froot, ckt_mRID)
