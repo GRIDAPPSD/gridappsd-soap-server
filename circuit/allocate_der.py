4c74a00de9521bf07514ce9555024ee455004e67
temcdrm <Thomas.McDermott@pnnl.gov>
2022-03-30 15:56:47 -0400

    updating the DER addition script for kWmax, category, and control mode

--- circuit/allocate_der.py
+++ circuit/allocate_der.py
@@ -1,6 +1,7 @@
 from SPARQLWrapper import SPARQLWrapper2#, JSON
 import cimhub.CIMHubConfig as CIMHubConfig
 import sys
+import math
 import numpy as np
 
 def create_der_list (cfg_file, froot, mRID, outpath, pv_size, pv_penetration):
@@ -12,20 +13,30 @@ def create_der_list (cfg_file, froot, mRID, outpath, pv_size, pv_penetration):
   np.random.seed (0)
   numpv = 0
   kw = 0.001 * pv_size
-  kva = kw / 0.898
+  kwmax = kw
+  kva = kwmax / math.sqrt(1-0.25*0.25)
   kv = 0.208
   kvar = 0.0
+  cat = 'catA'
+  mode = 'CQ'
 
   op = open (froot + '_der.dat', 'w')
   print ('uuid_file,{:s}_der_uuid.dat'.format (froot), file=op)
   print ('feederID,{:s}'.format (mRID), file=op)
+  print ('//', file=op)
+  print ('//name,bus,phases(ABCs1s2),type(Battery,Photovoltaic,SynchronousMachine),kwMax,RatedkVA,RatedkV,kW,kVAR', file=op)
+  print ('//  kW is taken as maximum real power', file=op)
+  print ('//  for Battery,Photovoltaic: append IEEE 1547 category [catA/catB], control mode [CQ,PF,VV,VW,WVAR,AVR,VV_VW]', file=op)
+  print ('//  for Battery: append RatedkWH,StoredkWH', file=op)
   print ('// name,bus,phases(ABCs1s2),type(Battery,Photovoltaic,SynchronousMachine),RatedkVA,RatedkV,kW,kVAR,RatedkWH,StoredkWH', file=op)
+  print ('//', file=op)
   print ('// commercial-scale, three-phase DER connected to the primary feeder', file=op)
-  print ('PV_1,node_60,ABC,Photovoltaic,2230.0,4.16,2000.0,0.0', file=op)
-  print ('Bat_PV,node_60,ABC,Battery,2000.0,4.16,0.0,0.0,8000.0,4000.0', file=op)
-  print ('WTG_1,node_1,ABC,SynchronousMachine,2230.0,4.16,2000.0,0.0', file=op)
-  print ('Bat_WTG,node_1,ABC,Battery,2000.0,4.16,0.0,0.0,8000.0,4000.0', file=op)
-  print ('// adding rooftop PV sized {:.2f} kW at {:.2f}% penetration'.format (kw, 100.0 * pv_penetration), file=op)
+  print ('PV_1,    node_60, ABC, Photovoltaic,       2000.0, 2230.0, 4.16, 2000.0, 0.0, catB, CQ', file=op)
+  print ('Bat_PV,  node_60, ABC, Battery,            2000.0, 2000.0, 4.16,    0.0, 0.0, catB, CQ, 8000.0, 4000.0', file=op)
+  print ('WTG_1,   node_1,  ABC, SynchronousMachine, 2000.0, 2230.0, 4.16, 2000.0, 0.0', file=op)
+  print ('Bat_WTG, node_1,  ABC, Battery,            2000.0, 2000.0, 4.16,    0.0, 0.0, catB, CQ, 8000.0, 4000.0', file=op)
+  print ('//', file=op)
+  print ('// adding rooftop PV sized {:.2f} kW at {:.2f}% penetration'.format (kwmax, 100.0 * pv_penetration), file=op)
 
   fidselect = """ VALUES ?fdrid {\"""" + mRID + """\"}
    ?s c:Equipment.EquipmentContainer ?fdr.
@@ -60,10 +71,10 @@ ORDER by ?p ?name
     #    print (b['bus'].value,b['basev'].value,b['phases'].value,b['p'].value)
     if np.random.uniform (0, 1) <= pv_penetration:
       numpv += 1
-      print ('Rooftop_{:d},{:s},s1s2,Photovoltaic,{:.3f},{:.3f},{:.3f},{:.3f}'.format (numpv, b['bus'].value, kva, kv, kw, kvar), file=op)
+      print ('Rooftop_{:d},{:s},s1s2,Photovoltaic,{:.3f},{:.3f},{:.3f},{:.3f},{:.3f},{:s},{:s}'.format (numpv, b['bus'].value, kwmax, kva, kv, kw, kvar, cat, mode), file=op)
 
   op.close()
-  print ('added {:d} PV sized {:.2f} kW for {:.2f}% penetration'.format (numpv, kw, 100.0 * pv_penetration))
+  print ('added {:d} PV sized {:.2f} kW for {:.2f}% penetration'.format (numpv, kwmax, 100.0 * pv_penetration))
 
 # run from command line for GridAPPS-D interop circuit
 if __name__ == '__main__':
